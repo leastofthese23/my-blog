0f3da6aedc07f7c85d93990d273e8a6d67f1ec2a
Israel McCulley <israelmcculley@gmail.com>
2020-03-23 18:33:35 -0700

    First Commit

---
+++ src/build/precache-manifest.61e43057ec8d2cddf604ad750ce8f8e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c38d00ade9b84d292d0d",
+    "url": "/static/css/main.5426bd6f.chunk.css"
+  },
+  {
+    "revision": "c38d00ade9b84d292d0d",
+    "url": "/static/js/main.82af1d17.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cfd6bc51c997d6d65b24",
+    "url": "/static/js/2.084ab1cf.chunk.js"
+  },
+  {
+    "revision": "5bfcebfa15f14e41feeae9ac8087138d",
+    "url": "/index.html"
+  }
+];
